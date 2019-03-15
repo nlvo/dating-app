a31e377c61e079a17221322e7ac6c14dd66389d0
Lien Vo <nlvo@live.nl>
2019-03-15 20:32:36 +0100

    edit index and css

--- index.js
+++ index.js
@@ -1,26 +1,53 @@
-const express = require('express')
-const pug = require('pug')
-const app = express()
-const port = 3000
+var express = require('express')
+var app = express()
+var port = 3000
 
 app.set('view engine', 'pug');
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
-
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-
-app.get('/login', (req, res) => {
-  res.render('login');
-});
-
 app.use(express.static(__dirname + '/public'));
 
-app.use(function (req, res, next) {
-  return res.status(404).send('not found 404')
-})
+app
+  .get('/', home)
+  .get('/about', about)
+  .get('/login', login)
+  .get('/register', register)
+  .post('/register', createProfile)
+  .use(notFound)
 
-app.listen(port);
+  .listen(port, listening);
+ 
+function home (req, res) {
+  res.render('index');
+}
+
+function about (req, res) {
+  res.render('about');
+}
+
+function login (req, res) {
+  res.render('login');
+}
+
+function register (req, res) {
+  res.render('register');
+}
+
+function createProfile (req, res) {
+  var name = req.body.name,
+      description = req.body.description;
+
+  // input[name="username"]
+  // req.body.username ^
+  // form ...
+  // req.body ^
+
+  res.render('login', { name: name,  description: description});
+}
+
+function notFound (req, res, next) {
+  res.status(404).send('not found 404')
+}
+
+function listening() {
+  console.log('Listening on port: ' + port);
+}

d462d77dcb3714f66508dfc566884b6ac22379ef
Lien Vo <nlvo@live.nl>
2019-03-28 13:05:59 +0100

    edit mongodb url

--- index.js
+++ index.js
@@ -27,8 +27,7 @@ var express = require('express'),
 var db = null,
 	dbName = process.env.DB_NAME,
 	dbHost = process.env.DB_HOST,
-	dbPort = process.env.DB_PORT,
-	url = 'mongodb://' + dbHost + ':' + dbPort;
+	url = 'mongodb://' + dbHost;
 
 // connect to database (local)
 mongo.MongoClient.connect(url, { useNewUrlParser: true }, function (err, client) {
